5cb7143a9921263a3b52f3c43b938f74d829d659
chriztso <christophertso2017@u.northwestern.edu>
2019-01-05 16:22:46 -0800

    "Working on Linked List"

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,18 +2,33 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-
+  
   list.addToTail = function(value) {
+    var node = Node(value);
+    if(this.head === null){
+      this.tail = node;
+      this.head = node;
+      node.next = null;
+    }
+    else{
+  
+    } 
   };
 
   list.removeHead = function() {
+    var tempHead = this.head;
+    console.log('temphead:',tempHead);
+    this.head = this.head.next;
+    console.log('head:',this.head);
+    return tempHead;
   };
 
   list.contains = function(target) {
   };
 
   return list;
-};
+    }
+
 
 var Node = function(value) {
   var node = {};
