aeee8337ec4e724c0e4f1d2fc97b1f9261184766
chriztso <christophertso2017@u.northwestern.edu>
2019-01-05 21:28:27 -0800

    "Working on tree"

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -6,30 +6,44 @@ var LinkedList = function() {
   list.addToTail = function(value) {
     var node = Node(value);
     if(this.head === null){
-     this.head = node; 
-     this.tail = node;
+      this.head = node; 
+      this.tail = node;
     }
     else{
-    var current = this.head;
-    while(current.next !== null){
-      current = current.next;
-    }
-    current.next = node;
-    this.tail = node;
-    }  
-};
+      var current = this.head;
+      while(current.next !== null){
+        current = current.next;
+      }
+      current.next = node;
+      this.tail = node;
+      }  
+  };
 
   list.removeHead = function() {
     var tempHead = this.head;
     this.head = this.head.next;
-    return tempHead;
+    return tempHead.value;
   };
 
   list.contains = function(target) {
+    var current =  this.head
+    if(current.value === target){
+      return true; 
+    }
+    else{
+    var current =  this.head;
+     while(current.next){
+      current = current.next;
+      if(current.value === target){
+      return true;
+      }
+     }
+     return false; 
+    }
   };
 
   return list;
-    }
+}
 
 
 var Node = function(value) {
