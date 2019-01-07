6cba968e49cdcb34d96097e4d3d05012b5d539ed
chriztso <christophertso2017@u.northwestern.edu>
2019-01-06 18:07:43 -0800

    "Worked on set"

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,51 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+//this._storage[item] = item;
+if(_.indexOf(this._storage, item) === -1){
+  this._storage.push(item);
+  console.log(this._storage);
+}
 };
 
 setPrototype.contains = function(item) {
+/*for(var key in this._storage){
+  if(key === item){
+  return true;
+  } 
+}
+*/
+if(_.indexOf(this._storage, item) === -1){
+    return false;
+  }
+else{
+  return  true; 
+}
+//return _.indexOf(this._storage, item) === 1 ? true : false; 
 };
 
 setPrototype.remove = function(item) {
-};
+/*
+for(var key in this._storage){
+  if(key === item){
+  delete this._storage[key];
+  } 
+}*/
+for(var i = 0; i < this._storage.length; i++){
+  if(this._storage[i] === item){
+    this._storage.splice(i, 1);   
+  }
+
+}
+console.log(this._storage);
+}
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
