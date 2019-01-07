e7bf3adac3550db3a63a539f7a774256165485b5
chriztso <christophertso2017@u.northwestern.edu>
2019-01-06 18:05:29 -0800

    "Worked on binary search tree"

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,60 @@
 var BinarySearchTree = function(value) {
+var newBST = {};
+newBST.value = value ;
+newBST.left = null; 
+newBST.right = null; 
+_.extend(newBST, bstPrototype);
+return newBST;
 };
+
+
+
+
+var bstPrototype = {};
+
+bstPrototype.insert = function(value){
+  
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = BinarySearchTree(value);
+      
+      }
+      else{
+        return this.left.insert(value);
+   
+      }
+    }
+    if (value > this.value) {
+      if (this.right === null) {
+        this.right = BinarySearchTree(value);
+      
+      }
+      else{
+        return this.right.insert(value);
+     
+      }
+    }
+}
+
+
+bstPrototype.contains = function(value){
+  if(this.value === value){
+   return true; 
+ 
+  }
+  else if(this.value < value && this.right !== null){
+ 
+    return this.right.contains(value);
+  }
+  else if(this.value > value && this.left !== null){
+    return this.left.contains(value);
+  }
+  return false;
+  }
+
+bstPrototype.depthFirstLog = function(value){
+
+}
 
 
 /*
