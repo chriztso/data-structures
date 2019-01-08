58d012851141d6e6e7e74c5eaa8bbf80821cc174
chriztso <christophertso2017@u.northwestern.edu>
2019-01-07 20:48:29 -0800

    "Finished hash tables"

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,22 +1,54 @@
 
 
 var HashTable = function() {
+  
+  
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-};
+  var index = getIndexBelowMaxForKey(k, this._limit); 
+  var array1 = [];
+  array1.push([k,v]);
+  var storageArray = this._storage.get(index);
+  if (!storageArray) {
+    this._storage.set(index, array1);
+  }
+   else if (storageArray) {
+    for (var i = 0; i < storageArray.length; i++) {
+      if (storageArray[i][0] === k) {
+        storageArray[i][1] = v;
+      }  
+    }
+    if (storageArray[i] !== k) {
+      storageArray.push([k, v]);
+    }
+   }
+  };
+  
+
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var storage1 = this._storage.get(index);
+  for (var i = 0; i < storage1.length; i++) {
+    if (storage1[i][0] === k) {
+      return storage1[i][1];
+    }
+  }
 };
+
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+   var storage1 = this._storage.get(index);
+  for(var i = 0; i < storage1.length; i++){
+    if(storage1[i][0] === k){
+      storage1.splice(i,1);
+    }
+  }
 };
-
 
 
 /*
