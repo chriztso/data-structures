2cf73639a6ebd05187ceb1152ff2827cc2874929
chriztso <christophertso2017@u.northwestern.edu>
2019-01-08 20:53:09 -0800

    "Cleaned up code and formatting"

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,71 +2,66 @@
 
 // Instantiate a new graph
 var Graph = function() {
-
+  this.storage = {};
 };
-var Graph1 = new Graph();
+
+//O(1)
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-this[node] = [];
+  this.storage[node] = [];
 };
 
+//O(n)
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-for(var prop in this){
-  if(Number(prop) === node){
-   return true;
+  for (var prop in this.storage) {
+    if (Number(prop) === node) {
+      return true;
+    }
   }
- }
-return false;
+  return false;
 };
 
+//O(n2)
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-delete this[node];
-for(var prop in this){
-  for(var i = 0; i < this[prop].length; i++){
-    if(this[prop][i] === node){
-      this[prop].splice(i, 1);
+  delete this.storage[node];
+  for (var prop in this.storage) {
+    for (var i = 0; i < this.storage[prop].length; i++) {
+      if (this.storage[prop][i] === node) {
+        this.storage[prop].splice(i, 1);
+      }
     }
-  }
-} 
+  } 
+};
 
-}
+//O(n)
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
- var isFrom = _.contains(this[fromNode], toNode);
- var isTo = _.contains(this[toNode], fromNode);
- return isFrom && isTo;
-}
-
-
-// Connects two nodes in a graph by adding an edge between them.
-Graph.prototype.addEdge = function(fromNode, toNode) {
-  
- // if (fromNode !== undefined && toNode !== undefined) {
-   console.log(fromNode, this[fromNode], 'from')  
-  console.log(toNode,this[toNode], 'to')     
-this[fromNode].push(toNode);
-  //}
-  // if (fromNode !== undefined && toNode !== undefined) {
-  console.log(toNode,this[toNode], 'to') 
-  console.log(fromNode, this[fromNode], 'from') 
-    this[toNode].push(fromNode);
-    
-  // }
+  var isFrom = _.contains(this.storage[fromNode], toNode);
+  var isTo = _.contains(this.storage[toNode], fromNode);
+  return isFrom && isTo;
 };
 
 
+//O(n)
+// Connects two nodes in a graph by adding an edge between them.
+Graph.prototype.addEdge = function(fromNode, toNode) {   
+  this.storage[fromNode].push(toNode);
+  this.storage[toNode].push(fromNode);
+};
+
+//O(n)
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  for (var i = 0; i < this[fromNode].length; i++) {
-    if (this[fromNode][i] === toNode) {
-      this[fromNode].splice(i, 1);
+  for (var i = 0; i < this.storage[fromNode].length; i++) {
+    if (this.storage[fromNode][i] === toNode) {
+      this.storage[fromNode].splice(i, 1);
     }
   }
-  for (var i = 0; i < this[toNode].length; i++) {
-    if (this[toNode][i] === fromNode) {
-      this[toNode].splice(i, 1);
+  for (var i = 0; i < this.storage[toNode].length; i++) {
+    if (this.storage[toNode][i] === fromNode) {
+      this.storage[toNode].splice(i, 1);
     }
   }
 
@@ -74,13 +69,11 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
   
   
 
-
+//O(n)
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
-  for (var prop in this) {
-    if(!isNaN(parseInt(prop))){ 
+  for (var prop in this.storage) {
     cb(parseInt(prop));
-    }
   }
   
 };
