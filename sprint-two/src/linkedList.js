4379291e5e90e831afaa704c9799af2ff9c411a7
chriztso <christophertso2017@u.northwestern.edu>
2019-01-08 20:56:18 -0800

    "Cleaned up linked list code and formatting"

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,47 +3,45 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
   
+  // O(n)
   list.addToTail = function(value) {
     var node = Node(value);
-    if(this.head === null){
+    if (this.head === null) {
       this.head = node; 
       this.tail = node;
-    }
-    else{
+    } else {
       var current = this.head;
-      while(current.next !== null){
+      while (current.next !== null) {
         current = current.next;
       }
       current.next = node;
       this.tail = node;
-      }  
+    }  
   };
-
+  // O(1)
   list.removeHead = function() {
     var tempHead = this.head;
     this.head = this.head.next;
     return tempHead.value;
   };
-
+  // 0(n)
   list.contains = function(target) {
-    var current =  this.head
-    if(current.value === target){
+    var current = this.head;
+    if (current.value === target) {
       return true; 
-    }
-    else{
-    var current =  this.head;
-     while(current.next){
-      current = current.next;
-      if(current.value === target){
-      return true;
+    } else {
+      var current = this.head;
+      while (current.next) {
+        current = current.next;
+        if (current.value === target) {
+          return true;
+        }
       }
-     }
-     return false; 
+      return false; 
     }
   };
-
   return list;
-}
+};
 
 
 var Node = function(value) {
