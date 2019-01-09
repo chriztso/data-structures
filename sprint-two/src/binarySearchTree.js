931d42386109397fe094db34528009eddf94c117
chriztso <christophertso2017@u.northwestern.edu>
2019-01-08 20:55:25 -0800

    "Cleaned up binary search tree code and formatting"

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,10 +1,10 @@
 var BinarySearchTree = function(value) {
-var newBST = {};
-newBST.value = value ;
-newBST.left = null; 
-newBST.right = null; 
-_.extend(newBST, bstPrototype);
-return newBST;
+  var newBST = {};
+  newBST.value = value;
+  newBST.left = null; 
+  newBST.right = null; 
+  _.extend(newBST, bstPrototype);
+  return newBST;
 };
 
 
@@ -12,56 +12,48 @@ return newBST;
 
 var bstPrototype = {};
 
-bstPrototype.insert = function(value){
+//O(log n)
+bstPrototype.insert = function(value) {
   
-    if (value < this.value) {
-      if (this.left === null) {
-        this.left = BinarySearchTree(value);
-      
-      }
-      else{
-        return this.left.insert(value);
-   
-      }
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(value);
+    } else {
+      return this.left.insert(value);
     }
-    if (value > this.value) {
-      if (this.right === null) {
-        this.right = BinarySearchTree(value);
-      
-      }
-      else{
-        return this.right.insert(value);
-     
-      }
-    }
-}
-
-
-bstPrototype.contains = function(value){
-  if(this.value === value){
-   return true; 
- 
   }
-  else if(this.value < value && this.right !== null){
- 
+  if (value > this.value) {
+    if (this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      return this.right.insert(value);
+    }
+  }
+};
+
+//O(log n)
+bstPrototype.contains = function(value) {
+  if (this.value === value) {
+    return true; 
+  } else if (this.value < value && this.right !== null) {
     return this.right.contains(value);
-  }
-  else if(this.value > value && this.left !== null){
+  } else if (this.value > value && this.left !== null) {
     return this.left.contains(value);
   }
   return false;
-  }
+};
 
-bstPrototype.depthFirstLog = function(value){
-var func = value;
-func(this.value);
+//O(n)
+bstPrototype.depthFirstLog = function(value) {
+  var func = value;
+  func(this.value);
   if (this.left) {
     this.left.depthFirstLog(value);
   }
   if (this.right) {
-    this.right.depthFirstLog(value)
+    this.right.depthFirstLog(value);
   }
-}
+};
 
 
 /*
